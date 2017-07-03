516f221ee9e425712b53836a8cb6f421542825d3
LikeJust <diddydwns135@gmail.com>
2017-07-03 13:36:02 +0900

    ???

--- tasks.py
+++ tasks.py
@@ -4,19 +4,24 @@ import requests
 
 def echo_response(message):
     if message["type"] == "message":
-        if message['text'] == '비트 코인 시세':
-            url = "https://api.korbit.co.kr/v1/ticker/detailed"
+        ReplyToActivity(fill=message,
+                        text=message["text"]).send()
 
-            headers = {
-                'cache-control': "no-cache",
-                'postman-token': "3cb8f5cc-b1b2-48de-9086-389941e47ab1"
-            }
-
-            response = requests.request("GET", url, headers=headers)
-
-            print(response.text)
-            ReplyToActivity(fill=message,
-                            text=response.text).send()
-        else:
-            ReplyToActivity(fill=message,
-                            text=message).send()
+# def echo_response(message):
+#     if message["type"] == "message":
+#         if message['text'] == '비트 코인 시세':
+#             url = "https://api.korbit.co.kr/v1/ticker/detailed"
+#
+#             headers = {
+#                 'cache-control': "no-cache",
+#                 'postman-token': "3cb8f5cc-b1b2-48de-9086-389941e47ab1"
+#             }
+#
+#             response = requests.request("GET", url, headers=headers)
+#
+#             print(response.text)
+#             ReplyToActivity(fill=message,
+#                             text=response.text).send()
+#         else:
+#             ReplyToActivity(fill=message,
+#                             text=message).send()
