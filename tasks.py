ce68aca2ea9c6ffd7cf17ffd2cff0e8d57e03c63
LikeJust <diddydwns135@gmail.com>
2017-07-03 12:13:26 +0900

    'how?'

--- tasks.py
+++ tasks.py
@@ -1,6 +1,18 @@
 from microsoftbotframework import ReplyToActivity
+import requests
 
 def echo_response(message):
   if message["type"] == "message":
-    ReplyToActivity(fill=message,
-                    text=message["text"]).send()
+    if message['text'] == '비트 코인 시세':
+      url = "https://api.korbit.co.kr/v1/ticker/detailed"
+
+      headers = {
+          'cache-control': "no-cache",
+          'postman-token': "3cb8f5cc-b1b2-48de-9086-389941e47ab1"
+          }
+
+      response = requests.request("GET", url, headers=headers)
+
+      print(response.text)
+      ReplyToActivity(fill=message,
+                      text=response.text).send()
