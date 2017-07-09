5cc283bdadb9ec6d34b1bf21217525142f5de80c
Name <diddydwns135@gmail.com>
2017-07-09 22:20:20 +0900

    'port_edit'

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from microsoftbotframework import MsBot
 from tasks import *
 import os
 
-# bot = MsBot(port=int(os.environ['PORT']))
-bot = MsBot(port=3978)
+bot = MsBot(port=int(os.environ['PORT']))
+# bot = MsBot(port=3978)
 bot.add_process(echo_response)
 bot.run()
